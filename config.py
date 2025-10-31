e1372233efbe32475a00987090b127f4fb4adef0
ROHIT12-bit <reddyrohit823@gmail.com>
2025-10-31 21:14:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# 𝐌𝐀𝐃𝐄 𝐁𝐘 𝐑𝐎𝐇𝐈𝐓 | 𝐁𝐎𝐓𝐒𝐊𝐈𝐍𝐆𝐃𝐎𝐌𝐒
+# 𝐓𝐆 𝐈𝐃 : @𝐁𝐎𝐓𝐒𝐊𝐈𝐍𝐆𝐃𝐎𝐌𝐒
+# 𝐀𝐍𝐘 𝐈𝐒𝐒𝐔𝐄𝐒 𝐎𝐑 𝐀𝐃𝐃𝐈𝐍𝐆 𝐌𝐎𝐑𝐄 𝐓𝐇𝐈𝐍𝐆𝐒 𝐂𝐀𝐍 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐌𝐄
+
+# ---------- EDIT THESE BEFORE DEPLOY ----------
+BOT_TOKEN   = "PUT_YOUR_BOT_TOKEN_HERE"    # from @BotFather
+API_ID      = 123456                       # from https://my.telegram.org
+API_HASH    = "your_api_hash_here"         # from https://my.telegram.org
+
+OWNER_USERNAME = "@BOTSKINGDOMS"
+
+MAIN_CHANNEL = "https://t.me/BOTSKINGDOMS"
+SUPPORT_LINK = "https://t.me/BOTSKINGDOMSGROUP"
+WELCOME_IMAGE = "https://i.ibb.co/bMFcCB6B/59kLh.jpg"
+
+FOOTER = "Powered by @BOTSKINGDOMS ⚡"
+
+LOG_CHAT_ID = 0
+AUTO_APPROVE_INSTANT = True
+SEND_WELCOME_IN_CHAT = True
